ede6b59fad9d874a200771c6f5fe5a045fbb712c
Martin Delcourt <martin.delcourt@cern.ch>
2022-09-22 13:20:18 +0200

    Initial commit

---
+++ IIHEPhotoDB.py
@@ -0,0 +1,12 @@
+
+
+
+class IIHEPhotoDB:
+    def __init__(self, IP):
+        pass
+    def getListOfFolder(self):
+        pass
+    def createFolder(self, folder_name):
+        pass
+    def uploadImage(self, image_path, db_folder, metadata):
+        pass
