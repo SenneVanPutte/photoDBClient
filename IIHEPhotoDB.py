56ee9b8d56daf16c053c906766f18edb33d6d113
dhohov <dmytro.hohov@cern.ch>
2022-09-22 13:26:53 +0200

    test

--- IIHEPhotoDB.py
+++ IIHEPhotoDB.py
@@ -6,6 +6,7 @@ class IIHEPhotoDB:
         pass
     def getListOfFolder(self):
         pass
+        # Hello
     def createFolder(self, folder_name):
         pass
     def uploadImage(self, image_path, db_folder, metadata):
